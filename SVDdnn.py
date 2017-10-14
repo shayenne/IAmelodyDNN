e57c46df5e9495002d26cefd085c92506604b1f3
Shayenne da Luz Moura <shayenne@ime.usp.br>
2017-10-14 19:42:07 -0300

    SVD train works and Making predictions with new tests

--- SVDdnn.py
+++ SVDdnn.py
@@ -1,31 +1,36 @@
-from __future__ import print_function
-import numpy as np
+# Create network with Keras
 from keras.preprocessing import sequence
-from keras.models import Model, Sequential
-from keras.layers import Dense, Dropout, Embedding, LSTM, Input, add, Concatenate, merge, Add,Bidirectional, Activation, concatenate
-from keras.models import model_from_json
+from keras.initializers import RandomNormal
+from keras.models import Model, Sequential, model_from_json
+from keras.layers import Dense, Dropout, Embedding, LSTM, Add, Bidirectional
+from keras import optimizers
 import keras.layers
 import keras
-import numpy
-import glob, os
+import numpy as np
 import pandas as pd
-np.random.seed(7)  # for reproducibility
+import glob
+import os
+
+
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
 
 
 # Read all files in path and concatenate them 
 print("Loading features and labels for training...")
 path_mfcc =r'/var/tmp/IA/mfcc/' # use your path
 path_label =r'/var/tmp/IA/labels/' # use your path
-#allFiles = glob.glob(path + "/*mfcc.csv")
+
 list_mfcc = []
 list_labels = []
 
 i = 0
 for filename in os.listdir(path_mfcc):
-    
+    if i > 2:
+        break
     if filename.endswith("mfcc.csv"):
         music = filename
-        #music = "MusicDelta_Reggae_"
         music = music[:-8]
         print (music)
         print("   Load", path_mfcc+music+"mfcc.csv")
@@ -35,76 +40,74 @@ for filename in os.listdir(path_mfcc):
         d2 = pd.read_csv(path_label+music+"labels.csv",index_col=None, header=None)
         list_labels.append(d2)
     i +=1
-    if i == 5:
-        break
-
+    
+# Grouping data 
 frame = pd.concat(list_mfcc)
 label = pd.concat(list_labels)
 
 
+# Formating data
 df2=pd.DataFrame.as_matrix(frame)
-print (df2.shape)
-# Trying to rescale the data
-df2 = df2 - df2.mean()
-df2 = df2 / df2.var()**2
-
-
 df4=pd.DataFrame.as_matrix(label)
 df4[df4 > 0] = 1
-print (df4.shape)
+print("Data's shape:", df2.shape)
+print("Data's shape:", df4.shape)
 
+
+# Rescale the data over all training set
+meanDF = df2 - np.mean(df2, axis=0).reshape(1, df2.shape[1])
+stdDF  = meanDF / np.std(meanDF, axis=0).reshape(1, df2.shape[1])
 print("Data's shape:", df2.shape)
 
-# create data
+
+# Train and test data
 x_train = df2
 y_train = df4
 
-# Important variables
-max_features = 120
-maxlen = 120
-batch_size = 1000
-
+# Padding sequences for input 
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 
-# create model
+
+# Important variables
+max_features = 120
+hidden_neurons = 50
+maxlen = 120
+
+
+# Create model
 print("Create model...")
 model = Sequential()
 model.add(Embedding(max_features, 50, input_length=maxlen))
-model.add(Bidirectional(LSTM(50, return_sequences=True)))
-model.add(Bidirectional(LSTM(50, return_sequences=True)))
-model.add(Bidirectional(LSTM(50)))
+model.add(Bidirectional(LSTM(50, return_sequences=True, kernel_initializer = RandomNormal(mean = 0, stddev = 0.1))))
+model.add(Bidirectional(LSTM(50, return_sequences=True, kernel_initializer = RandomNormal(mean = 0, stddev = 0.1))))
+model.add(Bidirectional(LSTM(50, kernel_initializer = RandomNormal(mean = 0, stddev = 0.1))))
 #model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
 
+
 # Compile model
-# 20 epochs without change
 print("Compile model...")
-model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer='adam')
+# The adam optimizer was used to reduce implementation details
 
 
 # Fit the model
 print("Starting model training...")
-#callbacks = [
-#    EarlyStoppingByLossVal(monitor='val_loss', value=0.00001, verbose=1),
-#    # EarlyStopping(monitor='val_loss', patience=2, verbose=0),
-#    ModelCheckpoint(kfold_weights_path, monitor='val_loss', save_best_only=True, verbose=0),
-#]
-
-for i in range(1):
+for i in range(25):
     model.fit(x_train, y_train,
-              batch_size=batch_size,
+              batch_size=100,
               epochs=1,
               validation_split=0.1, shuffle=True) 
-              #verbose=1, callbacks=callbacks)
-    print("> Finished", i , "epochs")
-        # serialize model to JSON
+    print("> Finished", i+1 , "epochs")
+    # Serialize model to JSON
     model_json = model.to_json()
     with open("SVDmodel.json", "w") as json_file:
         json_file.write(model_json)
-    # serialize weights to HDF5
+    # Serialize weights to HDF5
     model.save_weights("SVDmodel.h5")
     print("> Saved model to disk")
           
-# evaluate the model          
+        
+# Evaluate the model          
 scores = model.evaluate(x_train, y_train)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
--- f0dnn.py
+++ f0dnn.py
@@ -2,41 +2,40 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
+from keras.initializers import RandomNormal
 import keras
-import numpy 
 import numpy as np
 import pandas as pd
 import glob
 import os
+
 # fix random seed for reproducibility
 seed = 7
-numpy.random.seed(seed)
+np.random.seed(seed)
 
 # Read all files in path and concatenate them 
 print("Loading features and labels for training...")
-path_mfcc =r'/var/tmp/IA/features/' # use your path
+path_features =r'/var/tmp/IA/features/' # use your path
 path_label =r'/var/tmp/IA/labels/' # use your path
-#allFiles = glob.glob(path + "/*mfcc.csv")
+
 list_mfcc = []
 list_labels = []
 
-i = 0
+# Loading files
 for filename in os.listdir(path_mfcc):
     
     if filename.endswith("features.csv"):
         music = filename
         music = music[:-12]
         print (music)
-        print("   Load", filename)
-        d1 = pd.read_csv(path_mfcc+filename,index_col=None, header=None)
+        print("   Load", path_features+music+"features.csv")
+        d1 = pd.read_csv(path_features+music+"features.csv",index_col=None, header=None)
         list_mfcc.append(d1)
         print("   Load", path_label+music+"labels.csv")
         d2 = pd.read_csv(path_label+music+"labels.csv",index_col=None, header=None)
         list_labels.append(d2)
-    i +=1
-    
         
-
+# Grouping data 
 frame = pd.concat(list_mfcc)
 label = pd.concat(list_labels)
 
@@ -53,46 +52,57 @@ df5[:df4.shape[0],:] = df4
 print("Data's shape:", df2.shape)
 print("Data's shape:", df5.shape)
 
+
 # Remove no vocal frames - train with only vocal frames
 print("Removing no vocal frames from train data...")
 no_vocal = np.where(df5[:,0] < 1)
 
 for i in reversed(no_vocal):
-    df2 = numpy.delete(df2, i, axis=0)
-    df5 = numpy.delete(df5, i, axis=0)
+    df2 = np.delete(df2, i, axis=0)
+    df5 = np.delete(df5, i, axis=0)
 
 print("Data's shape after remove:", df2.shape)
 print("Data's shape after remove:", df5.shape)
 
-## Train and test data
-X = df2
-Y = keras.utils.to_categorical(df5, num_classes=193)
 
-# create model
+# Train and test data
+X = df2
+Y = keras.utils.to_categorical(df5, num_classes=193) 
+# To categorical formats the target to vectors of 0s and 1 on correspondent class
+
+
+# Create model 
 print("Create model...")
 model = Sequential()
 model.add(Dense(500, input_dim=513, activation='sigmoid'))
 model.add(Dense(500, activation='sigmoid'))
 model.add(Dense(193, activation='softmax'))
 
+
 # Compile model
 print("Compile model...")
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# The adam optimizer was used to reduce implementation details
+
 
 # Fit the model
 print("Starting model training...")
-for i in range(1):
-    model.fit(X, Y, epochs=10, batch_size=100, validation_split=0.1, shuffle=True)
-    print("> Finished", i * 10, "epochs")
-    # serialize model to JSON
+for i in range(100):
+    model.fit(X, Y, 
+              epochs=1, 
+              batch_size=30, 
+              validation_split=0.1, 
+              shuffle=True)
+    print("> Finished", i+1 , "epochs")
+    # Serialize model to JSON
     model_json = model.to_json()
     with open("f0model.json", "w") as json_file:
         json_file.write(model_json)
-    # serialize weights to HDF5
+    # Serialize weights to HDF5
     model.save_weights("f0model.h5")
     print("> Saved model to disk")
- 
 
-# evaluate the model
+
+# Evaluate the model
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
